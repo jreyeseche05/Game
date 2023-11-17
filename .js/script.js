6e62272c47c6f131b87ab49ec6f6d2c4d2570fba
jreyeseche05 <echeverriajackie0520@gmail.com>
2023-11-17 03:18:50 -0500

    ><

--- .js/script.js
+++ .js/script.js
@@ -1,11 +1,12 @@
+let cardOne, cardTwo;
 const cards = document.querySelectorAll('.card');
 
-function reveal (e) {
-    let clickedCard = (e.target);
+function revealCard (e) {
+    let clickedCard = e.target;
     clickedCard.classList.add('reveal');
 }
 
 cards.forEach(card => {
-    card.addEventListener('click', reveal);
+    card.addEventListener('click', revealCard);
 
 });
