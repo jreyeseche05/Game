e10c6f119d8184b14d3dfb15d60846b378854ff7
jreyeseche05 <echeverriajackie0520@gmail.com>
2023-11-17 03:51:43 -0500

    add button

--- .js/script.js
+++ .js/script.js
@@ -4,6 +4,10 @@ const cards = document.querySelectorAll('.card');
 function revealCard (e) {
     let clickedCard = e.target;
     clickedCard.classList.add('reveal');
+
+    cardOne = clickedCard;
+    cardTwo = clickedCard;
+    console.log (cardOne, cardTwo);
 }
 
 cards.forEach(card => {
