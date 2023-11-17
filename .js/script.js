8af1ea022e2acdcc6dda8ed34c133bbfa6d90976
jreyeseche05 <echeverriajackie0520@gmail.com>
2023-11-17 04:48:31 -0500

    couldnt fix everything

--- .js/script.js
+++ .js/script.js
@@ -1,16 +1,51 @@
-let cardOne, cardTwo;
+let cardOne,cardTwo;
 const cards = document.querySelectorAll('.card');
 
 function revealCard (e) {
     let clickedCard = e.target;
     clickedCard.classList.add('reveal');
 
-    cardOne = clickedCard;
-    cardTwo = clickedCard;
-    console.log (cardOne, cardTwo);
+    if (clickedCard !== cardOne) {
+        clickedCard.classList.add('reveal');
+   
+     if (!cardOne) {
+        return cardOne = clickedCard;
+     }
+     cardTwo = clickedCard;
+
+     let cardOneImg = cardOne.querySelector('img').src,
+     cardTwoImg = cardTwo.querySelector('img').src;
+     matchCards(cardOneImg, cardTwoImg);
+    }
+}
+function matchCards(img1, img2) {
+    if (img1 == img2){
+        return alert("Greate job!! Kepp going.");
+
+    }
+    else {
+        alert("Try again!");
+
+    }
+
 }
 
-cards.forEach(card => {
+
+  cards.forEach(card => {
     card.addEventListener('click', revealCard);
 
 });
+
+
+
+
+
+let play = document.getElementById("play");
+function playMusic() {
+    let audio = new Audio("play.mp3");
+    audio.play();
+}
+play.addEventListener('click',playMusic);
+
+
+
