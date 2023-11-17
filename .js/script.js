39ee6138115aca34c5fe7c9e571d6b79c129c272
jreyeseche05 <echeverriajackie0520@gmail.com>
2023-11-17 03:11:41 -0500

    working on js

--- .js/script.js
+++ .js/script.js
@@ -1,45 +1,11 @@
-alert('Hello')
-
 const cards = document.querySelectorAll('.card');
-let cardOne,cardTwo;
 
-
-function flipCard(e){
-    let clickedCard = e.target;
-
-
-if (clickedCard !== cardOne) {
-    clickedCard.classList.add('flip');
-
-    if (!cardOne) {
-        return cardOne = clickedCard;
-    }
-    cardTwo = clickedCard;  
-    let cardOneImg = cardOne.querySelector('img').src,
-     cardTwoImg = cardTwo.querySelector('img').src;
-     matchCards(cardOneImg, cardTwoImg);
-
-}
-
-
-}
-
-function matchCards(img1, img2){
-    if (img1 == img2){
-        return alert('Great job! Keep going.');
-    }
-    else {
-        setTimeout( ) {
-            cardOne.classList.add('vibration');
-            cardTwo.classList.add('vibration');
-
-    }
-
-    }
-
-
+function reveal (e) {
+    let clickedCard = (e.target);
+    clickedCard.classList.add('reveal');
 }
 
 cards.forEach(card => {
-    card.addEventListener('click', flipCard);
+    card.addEventListener('click', reveal);
+
 });
